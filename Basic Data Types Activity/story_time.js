4029d138d0878f5fdfa2400ef69c8dc2298782ad
Aayushi Avabrath <aayushi.avabrath@gmail.com>
2022-07-22 00:12:43 -0700

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,30 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to the carnival!"
+let name = "Dusk";
+let title = "The Paradox Invader"
+let worldName = "Land of the Dark Moon"
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 2002
+let year = 700
+let numberOfInvitations = 1
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let carnivalAttractions = ["Haunted House", "Mysterious Party", "Upper-Side Dream Station"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let willHaveFun = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log (greetings + " My guest, I am the one and only " + name + ".");
+console.log("I am better known as the " + title);
+console.log("The " + worldName + " presents to you a variety of interesting attractions. ")
+console.log("In this world, the year is " + year + " and I am " + age + " years old. ")
+console.log("As my lucky guest, you are the special number " + numberOfInvitations + " roamer of our carnival! ")
+console.log("Invitee: Will I have fun here? ")
+console.log("Dusk: That is " + willHaveFun + ", you indeed will!! Check out our top-loved attraction, the " + carnivalAttractions[0] + ".")
+console.log("Dusk: Enjoy!")
